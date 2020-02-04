73c0d311e54409dca354579752b253dc0e2e9c73
jacoknapp <jacak@sbcglobal.net>
2020-02-04 00:17:48 -0600

    Update portainer.sh

--- portainer/rootfs/etc/cont-init.d/portainer.sh
+++ portainer/rootfs/etc/cont-init.d/portainer.sh
@@ -4,8 +4,3 @@
 # Runs some initializations for Portainer
 # ==============================================================================
 bashio::require.unprotected
-
-if bashio::config.has_value 'agentsecret' ; then
-    bashio::log.info 'Setting the Agent Secret'
-    export AGENT_SECRET=$(bashio::config 'agentsecret')
-fi
