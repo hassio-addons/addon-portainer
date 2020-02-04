1f382b018dae8ca2f501d9aadc6786f2b1b4b8a9
jacoknapp <jacoknapp@gmail.com>
2020-02-03 21:05:58 -0600

    Set the Agent Secret environment variable

--- portainer/rootfs/etc/cont-init.d/portainer.sh
+++ portainer/rootfs/etc/cont-init.d/portainer.sh
@@ -4,3 +4,8 @@
 # Runs some initializations for Portainer
 # ==============================================================================
 bashio::require.unprotected
+
+if bashio::config.has_value 'agentsecret' ; then
+    bashio::log.info 'Setting the Agent Secret'
+    export AGENT_SECRET=$(bashio::config 'agentsecret'
+fi
